f3e9d78f2011a8246e6415ac95b4ecbbcb24c896
ANS <36783010+answebdev@users.noreply.github.com>
2022-07-23 16:52:44 -0700

    add profile image

--- src/components/intro/Introduction.js
+++ src/components/intro/Introduction.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import './Introduction.css';
+import ProfileImage from '../../img/profile-img.png';
 
 const Introduction = () => {
   return (
     <div className='intro'>
-      <div className='intro-left'>Left</div>
-      <div className='intro-right'>Right</div>
+      <div className='intro-left'>
+        <div className='intro-left-wrapper'>
+          <h2 className='intro-intro'>Hello, my name is</h2>
+          <h1 className='intro-name'>Adolf Schmuck</h1>
+          <div className='intro-title'>
+            <div className='intro-title-wrapper'>
+              <div className='intro-title-item'>Email Developer</div>
+              <div className='intro-title-item'>Web Developer</div>
+              <div className='intro-title-item'>UI/UX Designer</div>
+              <div className='intro-title-item'>Photographer</div>
+              <div className='intro-title-item'>Writer</div>
+              <div className='intro-title-item'>Content Creator</div>
+            </div>
+          </div>
+          <div className='intro-description'>
+            I design and develop services for customers of all sizes,
+            specializing in creating stylish, modern websites, web services, and
+            online stores.
+          </div>
+        </div>
+      </div>
+      <div className='intro-right'>
+        <div className='i-bg'></div>
+        <img src={ProfileImage} alt='' className='intro-img' />
+      </div>
     </div>
   );
 };
