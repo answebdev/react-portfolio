c91c4cd4f6a35bfc5b3e4d16e89f65eaa53577c5
ANS <36783010+answebdev@users.noreply.github.com>
2022-07-23 15:41:22 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h3>
+          Adolf Schmuck
+          <br />
+          Portfolio Website
+        </h3>
+        <p>Coming Soon...</p>
+      </header>
+    </div>
+  );
+};
+
+export default App;
