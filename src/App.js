9201d634af1723c423c8ec4d42735cfd4d5b8310
ANS <36783010+answebdev@users.noreply.github.com>
2022-07-26 18:00:12 -0700

    add Context API

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-// import logo from './logo.svg';
+import { useContext } from 'react';
+import { ThemeContext } from './context';
 import Introduction from './components/Introduction/Introduction';
 import About from './components/About/About';
 import ProductList from './components/ProductList/ProductList';
@@ -12,8 +13,16 @@ import './App.css';
 // CSS Clip-Path Maker: https://bennettfeely.com/clippy/
 
 const App = () => {
+  const theme = useContext(ThemeContext);
+  const darkMode = theme.state.darkMode;
+
   return (
-    <div>
+    <div
+      style={{
+        backgroundColor: darkMode ? '#222222' : '#ffffff',
+        color: darkMode ? '#ffffff' : '#000000',
+      }}
+    >
       <Toggle />
       <Introduction />
       <About />
